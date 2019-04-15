e4a2d6e6b65bf2533e52ec9bcd359db0b464f597
Edward Wong <edwardlm.wong@gmail.com>
2019-04-16 04:08:03 +0800

    Async/await + parcel for proof of concept.

--- index-view.js
+++ index-view.js
@@ -11,10 +11,23 @@ class AppView {
   }
 
   setButtonError() {
-    document.querySelector("#btn-error").addEventListener('click', event => {
-      throw new Error('Uh-oh, something went wrong');
+    document.querySelector("#btn-error").addEventListener('click', async (event) => {
+      try {
+        await delayedError();
+      } catch (error) {
+        throw new Error('Uh-oh, something went wrong');
+      }
     });
   }
 }
+
+const delayedError = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () { 
+      console.log('reject!');
+      reject(); 
+    }, 2000);
+  });
+};
 
 export default AppView;
