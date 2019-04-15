8d4f4ea38923082208b21e63ce6702c1ea2bfa8d
Edward Wong <edwardlm.wong@gmail.com>
2019-04-16 07:03:00 +0800

    Expose LiveCard resources under the namespace of LiveCard. Note: things will start to get more compartmentalized to specific resources, going forward.

---
+++ livecard.sdk.js
@@ -0,0 +1,68 @@
+/**
+ * @file See {@link LiveCard} for library properties and methods
+ * @author LiveCard LLC
+ */
+
+const ErrorType = {
+  // This browser does not support recording videos
+  RECORDING_NOT_SUPPORTED: 0,
+
+  // The user did not record or select a video
+  NO_VIDEO_SELECTED: 1,
+
+  // The user did not snap or select a still image
+  NO_IMAGE_SELECTED: 2,
+
+  // The video recording failed
+  RECORDING_FAILED: 3,
+
+  // Attempting to save the card record server side failed
+  CREATE_CARD_ERROR: 4,
+
+  // If `requireRecipientPhone` was enabled, this indicates a phone was not collected prior to attempting to save
+  MISSING_PHONE: 5,
+
+  // No video was provided prior to attempting to save
+  MISSING_VIDEO: 6,
+
+  // No text gift message was provided prior to attempting to save
+  MISSING_TEXT: 7,
+
+  // No static image was provided prior to attempting to save
+  MISSING_IMAGE: 8,
+
+  // The attempt to confirm this card (after checkout) failed
+  CONFIRM_CARD_ERROR: 9
+};
+
+const Context = {
+  licenseKey: null,
+  
+  // (Optional) Controls whether the SDK requires a recipient phone to create a card. Set to `false` 
+  // if you do not plan to collect recipient phone for SMS delivery during the checkout process.
+  requireRecipientPhone: true,
+
+  isMobile: false,
+  usingFileInput: false,
+  liveCardId: "",
+  recordedBlobs: [],
+  snapshotDataUrl: null,
+  mediaRecorder: null,
+  giftMessage: "",
+  recipientPhone: "",
+  messageType: "text",
+  videoRecordSuccessCallback: null,
+  videoRecordFailureCallback: null,
+  imageChooseSuccessCallback: null,
+  imageChooseFailureCallback: null,
+  createCardSuccessCallback: null,
+  createCardFailureCallback: null,
+  phoneInputCallback: null,
+  giftTextInputCallback: null,
+  debug: false,
+};
+
+export {
+  ErrorType,
+  Context,
+};
