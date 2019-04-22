fd49679a8c3428dbc3c1da93b6bb53ca2ec7c227
Edward Wong <edwardlm.wong@gmail.com>
2019-04-23 07:24:49 +0800

    VideoModel wip. VideoModel is a wrapper class that manages support for webcam-based or flash-based video recording.

--- model/video.js
+++ model/video.js
@@ -1,44 +1,43 @@
-import Webcam from '../lib/webcam';
+import WebcamVideoRecorder from '../lib/webcam';
 
 class VideoModel {
   constructor() {
-    this.blobs = [];
-    this.mediaRecorder
+    this.recorder = null;
+  }
+
+  async init() {
+    // try webcam
+    try {
+      this.recorder = new WebcamVideoRecorder();
+      await this.recorder.init();
+      return Promise.resolve();
+    } catch(error) {
+      console.log(`Failed to init webcam - ${error}`);
+      return Promise.reject();
+    }
+
+    // TODO: try flash cam
   }
 
   start() {
-    this.blobs = [];
-    if (typeof window.MediaRecorder === 'undefined') {
-      try {
-        if (!MediaRecorder.isTypeSupported("video/webm;codecs=vp9")) {
-          console.log("video/webm;codecs=vp9 is not Supported");
-          if (!MediaRecorder.isTypeSupported("video/webm;codecs=vp8")) {
-            console.log("video/webm;codecs=vp8 is not Supported");
-            if (!MediaRecorder.isTypeSupported("video/webm")) {
-              console.log("video/webm is not Supported");
-            }
-          }
-        }
-        this.mediaRecorder = new MediaRecorder(window.stream, options);
-      } catch (e) {
-        console.error("Exception while creating MediaRecorder: " + e);
-        return;
-      }
-    }
-
-    console.log("Created MediaRecorder", this.mediaRecorder, "with options", options);
-    this.mediaRecorder.ondataavailable = this.onData.bind(this);
-    this.mediaRecorder.start(10); // collect 10ms of data
-    this.debugLog("MediaRecorder started", this.mediaRecorder);
-    document.querySelector("#btnRecord").style.display = "none";
-    document.querySelector("#btnStop").style.display = "inline";
+    this.recorder.recordStart();
   }
 
-  onData(event) {
-    if (typeof window.MediaRecorder === 'undefined') return;
-    if (event.data && event.data.size > 0) {
-      this.blobs.push(event.data);
-    }
+  stop() {
+    this.recorder.recordStop();
+  }
+
+  buffer() {
+    return this.bufferVideo();
+  }
+
+  reset() {
+    this.recorder.reset();
+  }
+
+  stageVideoForUpload() {
+    this.recorder.streamStop();
+    return this.recorder.hasData();
   }
 }
 
