b0b823634c25ecc2007834a36c85c0cb5256dd81
Edward Wong <edwardlm.wong@gmail.com>
2019-08-22 10:19:47 +0800

    enable swfobject.js wip

--- scripts/enable_swfobject.js
+++ scripts/enable_swfobject.js
@@ -2,9 +2,16 @@ const fs = require('fs');
 
 const encoding = 'utf-8';
 const fpath = './dist/dev.html';
-const t = fs.readFileSync(fpath, encoding);
-const tag = '<script src="/livecard-sdk/flash/swfobject.js"></script>';
-const output = t.replace(`<!-- ${tag} -->`, tag);
+console.log(`Watching for file changes on ${fpath}`);
 
-fs.unlinkSync(fpath)
-fs.writeFileSync(fpath, output, encoding);
+// fs.unlinkSync(fpath)
+fs.watchFile(fpath, (curr, prev) => {
+  const t = fs.readFileSync(fpath, encoding);
+  const tag = '<script src="/livecard-sdk/flash/swfobject.js"></script>';
+  const output = t.replace(`<!-- ${tag} -->`, tag);
+  fs.unlinkSync(fpath)
+  fs.writeFileSync(fpath, output, encoding);
+  console.log(`Updated ${fpath} to enable swfobject.js`);
+  process.exit(0);
+});
+
