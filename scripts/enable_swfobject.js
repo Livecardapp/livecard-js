34570e452d1e3927821dd42fb7633327ebc0fe1e
Edward Wong <edwardlm.wong@gmail.com>
2019-08-23 06:33:46 +0800

    Set dev.html to be an empty file at the start.

--- scripts/enable_swfobject.js
+++ scripts/enable_swfobject.js
@@ -4,14 +4,19 @@ const encoding = 'utf-8';
 const fpath = './dist/dev.html';
 console.log(`Watching for file changes on ${fpath}`);
 
-// fs.unlinkSync(fpath)
+if (fs.existsSync(fpath))
+  fs.unlinkSync(fpath);
+
+fs.closeSync(fs.openSync(fpath, 'w'));
+
 fs.watchFile(fpath, (curr, prev) => {
   const t = fs.readFileSync(fpath, encoding);
   const tag = '<script src="/livecard-sdk/flash/swfobject.js"></script>';
   const output = t.replace(`<!-- ${tag} -->`, tag);
   fs.unlinkSync(fpath)
   fs.writeFileSync(fpath, output, encoding);
-  console.log(`Updated ${fpath} to enable swfobject.js`);
+  console.log(`Updated ${fpath} to enable swfobject.js\n`);
+  console.log(`Server started at http://localhost:1234/`);
   process.exit(0);
 });
 
